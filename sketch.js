65b0548d1175c4cefe9fe214eab9a49c29a26513
Joaquim Gamero <gb.joaquim@gmail.com>
2021-12-02 16:50:11 +0100

    Precalculate the curve particles beforehand

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 // Initial time
 let t = 0;
 
+// Lissajous curve info, computed before actual drawing
+const curveInfo = [];
+
 // Resolution (px)
-let size = 400;
+let size = 800;
 
 // Stroke size
-let strokeRatioX = 2;
-let strokeRatioY = 2;
+let strokeRatioX = 8;
+let strokeRatioY = 6;
+let strokeSizeX;
+let strokeSizeY;
 
 // Directions
 // true => ascending, false => descending
@@ -17,21 +22,27 @@ let blueDir = true;
 // Cache 
 let firstPositionSaved = false;
 let firstPositionReached = false;
+let curvePrecomputingFinished = false;
+let curvePaintingFinished = false;
 let firstX;
 let firstY;
 let particleCounter = 0;
 
 // Thresholds
 let overlapThreshold = 1000;
-let positionThreshold = 3;
+let positionThreshold = 1.5;
 
 function setup() {
   // ********** Curves **********
   // Mathematical a, b values
   a = round(random(1, 10));
   b = round(random(1, 10));
+  colors = ["black"];
   // a = 8;
-  // b = 6;
+  // b = 5;
+
+  strokeSizeX = calculatePercent(strokeRatioX, size);
+  strokeRatioY = calculatePercent(strokeRatioY, size);
 
   // Randomize colors
   red = random(0, 255);
@@ -53,17 +64,33 @@ function setup() {
   button.mousePressed(stop);
 
   background(255);
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      let distanceFromTopLeft = dist(x, y, 0, 0) / ratio;
-      let distanceFromTopRight = dist(x, y, width, 0) / ratio;
-      let distanceFromBottomLeft = dist(x, y, 0, height) / ratio;
+  // for (let y = 0; y < height; y++) {
+  //   for (let x = 0; x < width; x++) {
+  //     let distanceFromTopLeft = dist(x, y, 0, 0) / ratio;
+  //     let distanceFromTopRight = dist(x, y, width, 0) / ratio;
+  //     let distanceFromBottomLeft = dist(x, y, 0, height) / ratio;
+  //     let distanceFromCenter = dist(x, y, width/2, height/2) / ratio;
+      
+  //     // stroke(
+  //     //   spicePosition == 1 ?
+  //     //     spice :
+  //     //     distanceFromCenter,
+  //     //   spicePosition == 2 ?
+  //     //     spice :
+  //     //     distanceFromTopRight,
+  //     //   spicePosition == 3 ?
+  //     //     spice :
+  //     //     distanceFromBottomLeft);
 
-      stroke(spicePosition == 1 ? spice : distanceFromTopLeft, spicePosition == 2 ? spice : distanceFromTopRight, spicePosition == 3 ? spice : distanceFromBottomLeft);
+  //     // stroke(
+  //     //   0,
+  //     //   0,
+  //     //   0);
 
-      point(x, y);
-    }
-  }
+
+  //     // point(x, y);
+  //   }
+  // }
 }
 
 function draw() {
@@ -73,38 +100,95 @@ function draw() {
     positionedX = width / 2 + x;
     positionedY = height / 2 + y;
 
-    stroke(red, green, blue);
-    fill(red, green, blue);
-
-    ellipse(positionedX, positionedY, calculatePercent(strokeRatioX, size), calculatePercent(strokeRatioY, size));
-
-    console.log('x: ', positionedX, 'y: ', positionedY, 'p: ', particleCounter, 't: ', t);
-
-    t += .001;
-
-    reassignColors();
+    curveInfo.push({
+      x: positionedX,
+      y: positionedY,
+      p: particleCounter,
+      t
+    });
 
     if (!firstPositionSaved) {
-      firstX = positionedX;
-      firstY = positionedY;
+        firstX = positionedX;
+        firstY = positionedY;
 
-      firstPositionSaved = true;
+        firstPositionSaved = true;
 
-      text('iX: ' + firstX.toString() + ', iY: ' + firstY.toString(), 10, 20);
-      text('a: ' + a.toString() + ', b: ' + b.toString(), 10, 30);
-    }
-
-    // console.log('Last X:', round(x), 'Last Y:', round(y));
+        text('iX: ' + firstX.toString() + ', iY: ' + firstY.toString(), 10, 20);
+        text('a: ' + a.toString() + ', b: ' + b.toString(), 10, 30);
+      }
 
     if (particleCounter > getCorrectOverlapThreshold() &&
       isInsideThreshold(positionedX, firstX, positionThreshold) &&
       isInsideThreshold(positionedY, firstY, positionThreshold)) {
-      console.log('Curve stopped at particle number', particleCounter);
+      
       firstPositionReached = true;
-      saveCanvas('lissa_jous_test', 'png');
+      curvePrecomputingFinished = true;
+
+
+      console.log('Curve pre-computing finished at particle n.', particleCounter);
+      console.log(curveInfo);
     }
 
+    t += .001;
     particleCounter++;
+    console.log('Computed particle n.', particleCounter);
+  }
+
+
+
+
+  if (curvePrecomputingFinished && !curvePaintingFinished) {
+    for (let i = 0; i < curveInfo.length; i++) {
+      const currentParticle = curveInfo[i];
+
+      stroke(red, green, blue);
+      fill(red, green, blue);
+      ellipse(currentParticle.x, currentParticle.y, strokeSizeX, strokeSizeY);
+
+      reassignColors();
+    }
+
+    curvePaintingFinished = true;
+
+
+
+    // y = calculatePercent(33, size) * sin(a * t + PI / 2);
+    // x = calculatePercent(33, size) * sin(b * t);
+    // positionedX = width / 2 + x;
+    // positionedY = height / 2 + y;
+
+    // stroke(red, green, blue);
+    // fill(red, green, blue);
+
+    // ellipse(positionedX, positionedY, calculatePercent(strokeRatioX, size), calculatePercent(strokeRatioY, size));
+
+    // console.log('x: ', positionedX, 'y: ', positionedY, 'p: ', particleCounter, 't: ', t);
+
+    // t += .001;
+
+    // reassignColors();
+
+    // if (!firstPositionSaved) {
+    //   firstX = positionedX;
+    //   firstY = positionedY;
+
+    //   firstPositionSaved = true;
+
+    //   text('iX: ' + firstX.toString() + ', iY: ' + firstY.toString(), 10, 20);
+    //   text('a: ' + a.toString() + ', b: ' + b.toString(), 10, 30);
+    // }
+
+    // // console.log('Last X:', round(x), 'Last Y:', round(y));
+
+    // if (particleCounter > getCorrectOverlapThreshold() &&
+    //   isInsideThreshold(positionedX, firstX, positionThreshold) &&
+    //   isInsideThreshold(positionedY, firstY, positionThreshold)) {
+    //   console.log('Curve stopped at particle number', particleCounter);
+    //   firstPositionReached = true;
+    //   saveCanvas('lissa_jous_test', 'png');
+    // }
+
+    // particleCounter++;
   }
 }
 
